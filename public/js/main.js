dab5dfe566abf0039f025f76eec02a72497233eb
Jeffrey Liang <jeffrey.liang@mCHSbmi-9CT6G12.asurite.ad.asu.edu>
2019-10-09 17:53:22 -0700

    cleaned up code some, pure black/white, select/switch/peek animations

--- public/js/main.js
+++ public/js/main.js
@@ -2,29 +2,81 @@ function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function dropClick() {
-	document.getElementsByClassName("drop")[0].removeEventListener("click", dropClick);
-	document.getElementsByClassName("fetch")[0].style.display = "none";
-	document.getElementsByClassName("drop")[0].style["background-color"] = "#24272e";
-	document.getElementsByClassName("drop")[0].style.color = "#ffffff";
-	document.getElementById("title").style.color = "#ffffff"
-	console.log("bruh2")
-	this.style.width = "100%"
+function setStyle(objId, propertyObject) {
+	var elem = document.getElementById(objId);
+	for (var property in propertyObject)
+    	elem.style[property] = propertyObject[property];
+}
+
+function cacheHover() {
+	setStyle("cache-pane", {"background-color":"#000000", "color":"#ffffff"});
+	setStyle("fetch-pane", {"background-color":"#ffffff", "color":"#000000"});
+	setStyle("title", {"color":"#ffffff"});
+}
+
+function fetchHover() {
+	setStyle("fetch-pane", {"background-color":"#000000", "color":"#ffffff"});
+	setStyle("cache-pane", {"background-color":"#ffffff", "color":"#000000"});
+	setStyle("title", {"color":"#000000"});
+}
+
+function cachePeekIn() {
+	setStyle("cache-pane", {"width":"6%"});
+	setStyle("fetch-pane", {"width":"94%"});
+}
+
+function cachePeekOut() {
+	setStyle("cache-pane", {"width":"5%"});
+	setStyle("fetch-pane", {"width":"95%"});
+}
+
+function fetchPeekIn() {
+	setStyle("fetch-pane", {"width":"6%"});
+	setStyle("cache-pane", {"width":"94%"});
+}
+
+function fetchPeekOut() {
+	setStyle("fetch-pane", {"width":"5%"});
+	setStyle("cache-pane", {"width":"95%"});
+}
+
+function cacheClick() {
+	var cacheElem = document.getElementById("cache-pane");
+	var fetchElem = document.getElementById("fetch-pane");
+	cacheElem.removeEventListener("mouseover", cacheHover);
+	fetchElem.removeEventListener("mouseover", fetchHover);
+	cacheElem.removeEventListener("mouseenter", cachePeekIn);
+	cacheElem.removeEventListener("mouseleave", cachePeekOut);
+	setStyle("cache-pane", {"background-color":"#000000", "color":"#ffffff", "width":"95%"});
+	setStyle("fetch-pane", {"background-color":"#ffffff", "color":"#000000", "width":"5%"});
+	setStyle("title", {"color":"#ffffff", "left":"1%"});
+	setStyle("cache-heading", {"opacity":"0", "visibility":"hidden"});
+	setStyle("fetch-heading", {"opacity":"0", "visibility":"hidden"});
+	fetchElem.addEventListener("mouseenter", fetchPeekIn);
+	fetchElem.addEventListener("mouseleave", fetchPeekOut);
 }
 
 function fetchClick() {
-	document.getElementById("title").style.transition = "color 0.5s";
-	document.getElementsByClassName("fetch")[0].removeEventListener("click", fetchClick);
-	document.getElementsByClassName("fetch")[0].removeEventListener("click", dropClick);
-	document.getElementsByClassName("drop")[0].style.display = "none"
-	console.log("bruh2")
-	this.style.width = "100%"
-	document.getElementById("title").style.color = "#ffffff"
+	var fetchElem = document.getElementById("fetch-pane");
+	var cacheElem = document.getElementById("cache-pane");
+	fetchElem.removeEventListener("mouseover", fetchHover);
+	cacheElem.removeEventListener("mouseover", cacheHover);
+	fetchElem.removeEventListener("mouseenter", fetchPeekIn);
+	fetchElem.removeEventListener("mouseleave", fetchPeekOut);
+	setStyle("fetch-pane", {"background-color":"#000000", "color":"#ffffff", "width":"95%"});
+	setStyle("cache-pane", {"background-color":"#ffffff", "color":"#000000", "width":"5%"});
+	setStyle("title", {"color":"#ffffff", "left":"81.5%"});
+	setStyle("fetch-heading", {"opacity":"0", "visibility":"hidden"});
+	setStyle("cache-heading", {"opacity":"0", "visibility":"hidden"});
+	cacheElem.addEventListener("mouseenter", cachePeekIn);
+	cacheElem.addEventListener("mouseleave", cachePeekOut);
 }
 
 async function main() {
-	document.getElementsByClassName("drop")[0].addEventListener("click", dropClick);
-	document.getElementsByClassName("fetch")[0].addEventListener("click", fetchClick);
+	document.getElementById("cache-pane").addEventListener("mouseover", cacheHover);
+	document.getElementById("fetch-pane").addEventListener("mouseover", fetchHover);
+	document.getElementById("cache-pane").addEventListener("click", cacheClick);
+	document.getElementById("fetch-pane").addEventListener("click", fetchClick);
 }
 
 main()
