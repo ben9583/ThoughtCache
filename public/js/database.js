42762299b159df4c0c0c8a536c9ad19df3a6e5ce
Ben Plate <Ben@Bens-Macbook-Air.local>
2019-10-12 16:06:05 -0700

    connected form ui and backend

--- public/js/database.js
+++ public/js/database.js
@@ -20,9 +20,27 @@ function createThought(msg) {
 }
 
 function gotData(data) {
-  console.log(data)
+  console.log(data);
 }
 
 function storeThought(m) { 
   database.ref("messages/" + (m.date)).set(m); 
 }
+
+document.getElementById("cache-submit").addEventListener("mousedown", function() {
+  var errorElem = document.getElementById("error-text") 
+  errorElem.innerHTML = "Submitting thought..."
+  errorElem.style.visibility = "visible"
+  var text = document.getElementById("cache-text").value
+  console.log(text.length)
+  if(text.length > 200) {
+    errorElem.innerHTML = "Thought is too long, maximum 200 characters (" + text.length + ")"
+    errorElem.style.color = "#ff9999"
+    return
+  } else {
+    var out = createThought(text)
+    storeThought(out)
+    errorElem.innerHTML = "Your thought has been submitted"
+    errorElem.style.color = "#99ff99"
+  }
+});
