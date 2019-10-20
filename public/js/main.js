6a76849b2bd1f619637d84c8c14f9e1f6dd52d2d
ben9583 <bplate9583@gmail.com>
2019-10-20 07:00:36 -0700

    overhauled info-pane
    not gonna lie this took many hours

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+var windowToggle = 0
+var infoToggle = false
+
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -12,99 +15,124 @@ function cacheHover() {
 	setStyle("cache-pane", {"background-color":"#1e3466", "color":"#ffffff"});
 	setStyle("fetch-pane", {"background-color":"#ffffff", "color":"#1e3466"});
 	setStyle("title", {"color":"#ffffff"});
-	setStyle("info", {"color":"#1e3466"});
+	setStyle("info", {"color":"#ffffff", "background-color": "#1e3466"});
 }
 
 function fetchHover() {
 	setStyle("fetch-pane", {"background-color":"#1e3466", "color":"#ffffff"});
 	setStyle("cache-pane", {"background-color":"#ffffff", "color":"#1e3466"});
 	setStyle("title", {"color":"#1e3466"});
-	setStyle("info", {"color":"#ffffff"});
+	setStyle("info", {"color":"#1e3466", "background-color": "#ffffff"});
 }
 
 function infoHover() {
-	setStyle("info", {"color":"#2a55a3"});
+	setStyle("info", {"background-color":"#092831", "color":"#ffffff"});
+}
+
+function infoHoverOut() {
+	if(windowToggle == 1) {
+		setStyle("info", {"background-color":"#1e3466", "color":"#ffffff"});
+	} else {
+		setStyle("info", {"background-color":"#ffffff", "color":"#1e3466"});
+	}
 }
 
 function infoPeekIn() {
-	setStyle("cache-pane", {"width":"6%"});
-	setStyle("fetch-pane", {"width":"94%"});
-	setStyle("info", {"opacity":"0", "visibility":"hidden"});
+	setStyle("cache-pane", {"width":"7%"});
+	setStyle("fetch-pane", {"width":"93%"});
+	/* setStyle("info", {"opacity":"0", "visibility":"hidden"}); */
 }
 
 function cachePeekIn() {
-	setStyle("cache-pane", {"width":"6%"});
-	setStyle("fetch-pane", {"width":"94%"});
-	setStyle("info", {"opacity":"0", "visibility":"hidden"});
+	setStyle("cache-pane", {"width":"8%"});
+	setStyle("fetch-pane", {"width":"92%"});
+	/* setStyle("info", {"opacity":"0", "visibility":"hidden"}); */
 }
 
 function cachePeekOut() {
-	setStyle("cache-pane", {"width":"5%"});
-	setStyle("fetch-pane", {"width":"95%"});
-	setStyle("info", {"opacity":"0", "visibility":"hidden"});
+	setStyle("cache-pane", {"width":"7%"});
+	setStyle("fetch-pane", {"width":"93%"});
+	/* setStyle("info", {"opacity":"0", "visibility":"hidden"}); */
 }
 
 function fetchPeekIn() {
-	setStyle("fetch-pane", {"width":"6%"});
-	setStyle("cache-pane", {"width":"94%"});
-	setStyle("info", {"opacity":"0", "visibility":"hidden"});
+	setStyle("fetch-pane", {"width":"8%"});
+	setStyle("cache-pane", {"width":"92%"});
+	/* setStyle("info", {"opacity":"0", "visibility":"hidden"}); */
 }
 
 function fetchPeekOut() {
-	setStyle("fetch-pane", {"width":"5%"});
-	setStyle("cache-pane", {"width":"95%"});
-	setStyle("info", {"opacity":"0", "visibility":"hidden"});
+	setStyle("fetch-pane", {"width":"7%"});
+	setStyle("cache-pane", {"width":"93%"});
+	/* setStyle("info", {"opacity":"0", "visibility":"hidden"}); */
 }
 
 function cacheClick() {
+	windowToggle = 1
 	var cacheElem = document.getElementById("cache-pane");
 	var fetchElem = document.getElementById("fetch-pane");
 	cacheElem.removeEventListener("mouseover", cacheHover);
 	fetchElem.removeEventListener("mouseover", fetchHover);
 	cacheElem.removeEventListener("mouseenter", cachePeekIn);
 	cacheElem.removeEventListener("mouseleave", cachePeekOut);
-	setStyle("cache-pane", {"background-color":"#1e3466", "color":"#ffffff", "width":"95%"});
-	setStyle("fetch-pane", {"background-color":"#ffffff", "color":"#1e3466", "width":"5%"});
+	setStyle("cache-pane", {"background-color":"#1e3466", "color":"#ffffff", "width":"93%"});
+	setStyle("fetch-pane", {"background-color":"#ffffff", "color":"#1e3466", "width":"7%"});
 	setStyle("title", {"color":"#ffffff", "left":"1.5%"});
+	setStyle("info", {"color":"#ffffff", "background-color": "#1e3466"});
 	setStyle("cache-heading", {"opacity":"0", "visibility":"hidden"});
 	setStyle("fetch-heading", {"opacity":"0", "visibility":"hidden"});
-	setStyle("cache-form", {"opacity":"1", "visibility":"visible", "transition":"opacity 0.5s"});
 	setStyle("cache-text", {"opacity":"1"});
+	setStyle("cache-form", {"display":"block"});
 	fetchElem.addEventListener("mouseenter", fetchPeekIn);
 	fetchElem.addEventListener("mouseleave", fetchPeekOut);
 }
 
 function fetchClick() {
+	windowToggle = 2
 	var fetchElem = document.getElementById("fetch-pane");
 	var cacheElem = document.getElementById("cache-pane");
 	fetchElem.removeEventListener("mouseover", fetchHover);
 	cacheElem.removeEventListener("mouseover", cacheHover);
 	fetchElem.removeEventListener("mouseenter", fetchPeekIn);
 	fetchElem.removeEventListener("mouseleave", fetchPeekOut);
-	setStyle("fetch-pane", {"background-color":"#1e3466", "color":"#ffffff", "width":"95%"});
-	setStyle("cache-pane", {"background-color":"#ffffff", "color":"#1e3466", "width":"5%"});
-	setStyle("title", {"color":"#ffffff", "left":"78.5%"});
+	setStyle("fetch-pane", {"color":"#ffffff", "background-color":"#1e3466", "width":"93%"});
+	setStyle("cache-pane", {"background-color":"#ffffff", "color":"#1e3466", "width":"7%"});
+	setStyle("title", {"color":"#ffffff", "left":"(100% - 10em)"});
+	setStyle("info", {"color":"#1e3466", "background-color": "#ffffff"});
 	setStyle("fetch-heading", {"opacity":"0", "visibility":"hidden"});
 	setStyle("cache-heading", {"opacity":"0", "visibility":"hidden"});
-	setStyle("cache-form", {"opacity":"0", "visibility":"hidden", "transition":"opacity 0.5s"});
+	setStyle("cache-text", {"opacity":"0"});
+	setStyle("cache-form", {"display":"none"});
 	cacheElem.addEventListener("mouseenter", cachePeekIn);
 	cacheElem.addEventListener("mouseleave", cachePeekOut);
 }
 
 function infoClick() {
 	var infoElem = document.getElementById("info");
-	infoElem.removeEventListener("mouseover", infoHover);
-	infoElem.removeEventListener("mouseenter", infoClick);
-	setStyle("cache-pane", {"visibility":"hidden"});
-	setStyle("fetch-pane", {"visibility":"hidden"});
-	setStyle("title", {"visibility":"hidden"});
-	setStyle("info-pane", {"visibility":"visible"});
+	if(!infoToggle) {
+		infoToggle = true
+		document.getElementById("cache-pane").removeEventListener("click", cacheClick);
+		document.getElementById("fetch-pane").removeEventListener("click", fetchClick);
+		//setStyle("cache-pane", {"visibility":"hidden"});
+		//setStyle("fetch-pane", {"visibility":"hidden"});
+		//setStyle("title", {"visibility":"hidden"});
+		setStyle("info-pane", {"top":"0"})
+	} else {
+		infoToggle = false
+		document.getElementById("cache-pane").addEventListener("click", cacheClick);
+		document.getElementById("fetch-pane").addEventListener("click", fetchClick);
+		setStyle("cache-pane", {"visibility":"visible"});
+		setStyle("fetch-pane", {"visibility":"visible"});
+		setStyle("title", {"visibility":"visible"});
+		setStyle("info-pane", {"top":"-40em"})
+	}
 }
 
 async function main() {
 	document.getElementById("cache-pane").addEventListener("mouseover", cacheHover);
 	document.getElementById("fetch-pane").addEventListener("mouseover", fetchHover);
 	document.getElementById("info").addEventListener("mouseover", infoHover);
+	document.getElementById("info").addEventListener("mouseleave", infoHoverOut);
 	document.getElementById("info").addEventListener("click", infoClick);
 	document.getElementById("cache-pane").addEventListener("click", cacheClick);
 	document.getElementById("fetch-pane").addEventListener("click", fetchClick);
